becf7c08b64f5cbc0e5ea66c22eac25342e34887
A.I Shifat <129369013+shifat100@users.noreply.github.com>
2025-04-11 17:37:23 +0600

    Update entoar.js

--- cloudfone/dictionaries/English Arbic/entoar.js
+++ cloudfone/dictionaries/English Arbic/entoar.js
@@ -6,7 +6,7 @@ var f2 = document.querySelectorAll('.footerelement')[1];
 var f3 = document.querySelectorAll('.footerelement')[2];
 var loader = document.getElementById('loader');
 
-function alert(msg="alertbox") { 
+function alert1(msg="alertbox") { 
 document.body.innerHTML+=('<div style="position:fixed; left:0px;bottom:0px;background:#e9e9e9;display:block;width:100%"><div style="padding: 10px;font-weight: bold">'+msg+'</div><div style="text-align:center;display:block;padding:5px; background:black; color: #e9e9e9">OK</div></div>');
  document.body.addEventListener('keydown', function(){window.location.reload();});
  }
@@ -27,7 +27,7 @@ xhttp.onload = function () {
         document.body.removeEventListener('keydown', keydownwordlist);
         document.body.addEventListener('keydown', keydownmain);
     } else {
-        //alert('something error');
+        //alert1('something error');
     }
 }
 xhttp.open('GET', 'data.json', true);
@@ -157,9 +157,9 @@ function keydownmain(e) {
             break;
         case 'F2': window.location.href = '/index.html';
             break;
-        case 'SoftLeft': alert('A Dictionary App By Shifat100');
+        case 'SoftLeft': alert1('A Dictionary App By Shifat100');
             break;
-        case 'F1': alert('A Dictionary App By Shifat100');
+        case 'F1': alert1('A Dictionary App By Shifat100');
             break;
     }
 
