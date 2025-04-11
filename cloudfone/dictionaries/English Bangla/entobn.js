21a6902833cfc420fa3c4efd12c59fe52b5f8303
A.I Shifat <129369013+shifat100@users.noreply.github.com>
2025-04-11 19:33:42 +0600

    Update entobn.js

--- cloudfone/dictionaries/English Bangla/entobn.js
+++ cloudfone/dictionaries/English Bangla/entobn.js
@@ -6,6 +6,11 @@ var f2 = document.querySelectorAll('.footerelement')[1];
 var f3 = document.querySelectorAll('.footerelement')[2];
 var loader = document.getElementById('loader');
 
+function alert(msg="alertbox") { 
+document.body.innerHTML+=('<div style="position:fixed; left:0px;bottom:0px;background:#e9e9e9;display:block;width:100%"><div style="padding: 10px;font-weight: bold">'+msg+'</div><div style="text-align:center;display:block;padding:5px; background:black; color: #e9e9e9">OK</div></div>');
+ document.body.addEventListener('keydown', function(){window.location.reload();});
+ }
+
 var xhttp = new XMLHttpRequest();
 xhttp.onload = function () {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -117,7 +122,7 @@ function keydownwordlist(e) {
             document.body.removeEventListener('keydown', keydownwordlist);
             document.body.addEventListener('keydown', keydownmain);
             break;
-        case 'SoftLeft': document.querySelectorAll('.focusable')[0].focus();
+        case 'Escape': document.querySelectorAll('.focusable')[0].focus();
             break;
         case 'F1': document.querySelectorAll('.focusable')[0].focus();
             break;
@@ -154,7 +159,7 @@ function keydownmain(e) {
             break;
         case 'F2': window.location.href='/index.html';
             break;
-        case 'SoftLeft': alert('A Dictionary App By Shifat100');
+        case 'Escape': alert('A Dictionary App By Shifat100');
             break;
         case 'F1': alert('A Dictionary App By Shifat100');
             break;
