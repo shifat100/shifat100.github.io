1c5c6210873e0550d8223bd6fcf8e3d2bb7d2f57
A.I Shifat <129369013+shifat100@users.noreply.github.com>
2025-12-02 15:07:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -245,4 +245,12 @@ function lastupdate() {
     xhttp.open('GET', 'script.js', true);
     xhttp.send();
 }
-lastupdate()
+
+lastupdate();
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker Registered!', reg))
+      .catch(err => console.log('Service Worker registration failed', err));
+  });
+}
