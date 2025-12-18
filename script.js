d8254b406a72a971809cadb74275cc6b02c94d1b
A.I Shifat <129369013+shifat100@users.noreply.github.com>
2025-12-18 11:31:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -231,4 +231,4 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').catch(err => console.log('SW failed', err));
   });
-}
+        }
