82f99bc6f9747f2744637590b5711133e57cfdee
A.I Shifat <129369013+shifat100@users.noreply.github.com>
2026-06-25 21:34:24 +0600

    Update cloud.v1.ads-min.js

--- cloudfone/cloud-ads/cloud.v1.ads-min.js
+++ cloudfone/cloud-ads/cloud.v1.ads-min.js
@@ -8,12 +8,15 @@
     window.cloudAdsQueue = window.cloudAdsQueue || [];
 
     const bannerAds =[
-        { type: 'custom', imageUrl: 'https://shifat100.github.io/cloudfone/cloud-adsimages/banner/' + (Math.floor(Math.random() * 9) + 1) + '.png', clickUrl: 'https://matcheshonoraryunderwater.com/h3ghsxyvp?key=331819c57a0e4e6203da3f03fe993d20' },
+        { type: 'custom', imageUrl: 'https://shifat100.github.io/cloudfone/cloud-adsimages/banner/' + (Math.floor(Math.random() * 9) + 1) + '.png', clickUrl: 'https://omg10.com/4/9569147/'
+            */'https://matcheshonoraryunderwater.com/h3ghsxyvp?key=331819c57a0e4e6203da3f03fe993d20'*/
+            },
         { type: 'adsterra', adsterraKey: '3768c5dda0b47669346bd50d7189ab3b', adsterraSrc: 'https://matcheshonoraryunderwater.com/3768c5dda0b47669346bd50d7189ab3b/invoke.js', width: 468, height: 60 }
     ];
 
     const fullscreenAds =[
-        { clickUrl: 'https://matcheshonoraryunderwater.com/h3ghsxyvp?key=331819c57a0e4e6203da3f03fe993d20' }
+        { clickUrl: 'https://omg10.com/4/9569147'
+         /*'https://matcheshonoraryunderwater.com/h3ghsxyvp?key=331819c57a0e4e6203da3f03fe993d20'*/ }
     ];
 
     const getRandomFullscreenImage = () => 'https://shifat100.github.io/cloudfone/cloud-ads/images/fullscreen/' + (Math.floor(Math.random() * 15) + 1) + '.png';
